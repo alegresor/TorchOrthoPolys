ea01018d2815e388b9a51255d04909f376cc932b
alegresor <agsorokin3@gmail.com>
2025-11-05 16:36:20 -0800

    comb func

---
+++ torchorthopolys/util.py
@@ -0,0 +1,33 @@
+import torch 
+import numpy as np 
+
+def lcomb(n, k):
+    return torch.lgamma(n+1)-torch.lgamma(k+1)-torch.lgamma(n-k+1)
+
+def comb(n, k):
+    """
+    Examples:
+        >>> import scipy.special
+        >>> n = torch.arange(5)
+        >>> k = torch.arange(4) 
+        >>> comb_torch = comb(n[:,None],k[None,:])
+        >>> comb_torch
+        tensor([[1., 0., 0., 0.],
+                [1., 1., 0., 0.],
+                [1., 2., 1., 0.],
+                [1., 3., 3., 1.],
+                [1., 4., 6., 4.]])
+        >>> comb_sp = scipy.special.comb(n[:,None].numpy(),k[None,:].numpy())
+        >>> np.allclose(comb_sp,comb_torch.numpy())
+        True
+        >>> n = torch.tensor([torch.pi])
+        >>> k = torch.tensor([torch.e])
+        >>> np.allclose(comb(n,k),scipy.special.comb(n.numpy(),k.numpy()))
+        True
+        >>> np.allclose(comb(k,n),scipy.special.comb(k.numpy(),n.numpy()))
+        True
+    """
+    v = torch.exp(lcomb(n,k))
+    v[n<k] = 0
+    return v
+
